9571af71b582171a1ffe3a277cc4629001d8a2ad
GUGAPRIYAA <115546633+Gugasivakumar0206@users.noreply.github.com>
2025-11-18 16:24:54 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -110,11 +110,13 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 })
 
 //Server
+//Server
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
+
 
 
 {/*
